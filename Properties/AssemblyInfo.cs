375a862cdaa1ed6dff4eefd6933ece51024886a6
class <class@PC16-401-12>
2024-02-19 16:56:28 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectAzimut")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectAzimut")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62b7beff-9df2-4f92-b89c-bf0b03022b67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+using Azimut;
+using System.Collections.Generic;
+
+namespace UnitTestProjectAzimut
+{
+    [TestClass]
+    public class UnitTestPoint
+    {
+        [DataRow(-1000.0, -1000.0)]
+        [DataRow(-1000.0, 1000.0)]
+        [DataRow(1000.0, -1000.0)]
+        [DataRow(1000.0, 1000.0)]
+        [DataRow(-90.01, -180.1)]
+        [DataRow(-90.01, 180.1)]
+        [DataRow(90.01, -180.1)]
+        [DataRow(90.01, 180.1)]
+        [DataTestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodCtor_ThrowsArgumentOutOfRangeException(double a, double b)
+        {
+            Point p;
+            p = new Point(a, b);
+        }
+        [DataRow(-89.01, -179.01)]
+        [DataRow(-89.01, 179.01)]
+        [DataRow(89.01, -179.01)]
+        [DataRow(89.01, 179.01)]
+        [DataTestMethod]
+        public void TestMethodCtor_CreatePoint(double a, double b)
+        {
+            Point p;
+            p = new Point(a, b);
+            Assert.IsNotNull(p);
+        }
+    }
+
+    [TestClass]
+    public class UnitTestDistanceAzimuth
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+        public void TestMethodCtor_CreateObject(Point a, Point b)
+        {
+           
+            DistanceAzimuth da;
+            da = new DistanceAzimuth(a, b);
+            Assert.AreEqual(da.A.Latitude, a.Latitude);
+            Assert.AreEqual(da.B.Latitude, b.Latitude);
+            Assert.AreEqual(da.A.Longitude, a.Longitude);
+            Assert.AreEqual(da.B.Longitude, b.Longitude);
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[] { new Point(70.0, 80.0), new Point(89.0, 179.0) };
+            yield return new object[] { new Point(89.0, 179.0), new Point(-89.0, -179.0) };
+        }
+    }
+    
+}
